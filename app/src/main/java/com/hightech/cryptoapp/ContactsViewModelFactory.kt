1cf3e65e2bef6971afac6dce3c496b8cd2cb7ac7
Syntax <fiqrihafzainislami.com>
2024-03-18 21:15:37 +0700

    Create `AvengerViewModel` class to create instance of AvengerViewModel

---
+++ app/src/main/java/com/hightech/cryptoapp/ContactsViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.hightech.cryptoapp
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.hightech.cryptoapp.LoadAvengerLocalUseCaseFactory.Companion.createLoadAvengerLocalUseCase
+import com.hightech.cryptoapp.presentation.AvengerViewModel
+
+val FACTORY: ViewModelProvider.Factory = viewModelFactory {
+    initializer {
+        AvengerViewModel(
+            useCase = createLoadAvengerLocalUseCase()
+        )
+    }
+}
